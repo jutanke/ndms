806ab89ff61b6818bf776abdbfc9a25cc46339a2
tanke <jutanke@googlemail.com>
2022-08-09 20:09:13 +0200

    add caching

--- ndms/database.py
+++ ndms/database.py
@@ -29,27 +29,44 @@ class Database:
         data: Data,
         kernel_size: int,
         transform_data_fn=None,
+        cache_fname: str = None,
     ):
+        if cache_fname is not None and not cache_fname.endswith(".npz"):
+            cache_fname = cache_fname + ".npz"
+
         self.kernel_size = kernel_size
         self.transform_data_fn = transform_data_fn
 
-        self.Meta = []
-        self.Seqs = []
-        self.Orig_Seqs = []
+        if cache_fname is not None and isfile(cache_fname):
+            obj = np.load(cache_fname)
+            self.Meta = obj["Meta"]
+            self.Seqs = obj["Seqs"]
+            self.Orig_Seqs = obj["Orig_Seqs"]
+        else:
+            self.Meta = []
+            self.Seqs = []
+            self.Orig_Seqs = []
+            for seqid in range(len(data)):
+                seq = data[seqid]
 
-        for seqid in range(len(data)):
-            seq = data[seqid]
+                total_dim = seq.shape[1]
+                assert int(m.ceil(total_dim / 3)) == int(m.floor(total_dim / 3))
 
-            total_dim = seq.shape[1]
-            assert int(m.ceil(total_dim / 3)) == int(m.floor(total_dim / 3))
+                for frame in range((len(seq) - kernel_size) + 1):
+                    sub_seq = seq[frame : frame + kernel_size].copy().astype("float32")
+                    self.Orig_Seqs.append(sub_seq)
+                    if transform_data_fn is not None:
+                        sub_seq = transform_data_fn(sub_seq)
+                    self.Seqs.append(sub_seq.flatten())
+                    self.Meta.append((seqid, frame))
 
-            for frame in range((len(seq) - kernel_size) + 1):
-                sub_seq = seq[frame : frame + kernel_size].copy().astype("float32")
-                self.Orig_Seqs.append(sub_seq)
-                if transform_data_fn is not None:
-                    sub_seq = transform_data_fn(sub_seq)
-                self.Seqs.append(sub_seq.flatten())
-                self.Meta.append((seqid, frame))
+            if cache_fname is not None:
+                np.savez(
+                    cache_fname,
+                    Seqs=self.Seqs,
+                    Meta=self.Meta,
+                    Orig_Seqs=self.Orig_Seqs,
+                )
 
         n_dim = len(self.Seqs[0])
         self.lookup = AnnoyIndex(n_dim, "euclidean")
